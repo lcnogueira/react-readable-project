12682a0e0487a07605a0be041c5138dc4ae2c4d4
Luiz Claudio <luizcns18@gmail.com>
2017-12-07 18:09:55 -0300

    Initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Categories</h1>
+        <h2>React</h2>
+        <h2>Redux</h2>
+        <h2>Udacity</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
