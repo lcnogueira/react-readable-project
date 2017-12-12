88628b498bd594ff642acf7b6bee96797fa967ec
Luiz Claudio <luizcns18@gmail.com>
2017-12-12 14:57:18 -0300

    feat: Add routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import '../styles/App.css';
 import Home from './Home';
 import Category from './Category';
+import Menu from './Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
-        <Category />
+
+        <Route exact path="/" component={Home} />
+
+        <Route path="/category" component={Category} />
+
       </div>
     );
   }
