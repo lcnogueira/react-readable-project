c1cb38ee58bcd2d4172e6ea781426edeb7762466
Luiz Claudio <luizcns18@gmail.com>
2017-12-12 16:59:39 -0300

    feat: Load categories from server

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as Api from '../utils/Api';
 
 class Menu extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            categories: [],
+        };
+    };
+
+    componentDidMount() {
+        Api.getCategories().then((categories) => {
+            this.setState({ categories });
+        })
+    };
+
     render() {
+
+        const {categories} = this.state;
+
         return (
             <div id="menu">
                 <ol>
                     <Link to="/">
                         <li>Home</li>
                     </Link>
-                    <Link to="/category">
-                        <li>Category</li>
-                    </Link>
+                    {categories && categories.map((category) => (
+                        <Link to={category.path} key={category.name}>
+                            <li>{category.name}</li>
+                        </Link>
+                    ))}
                 </ol>
             </div>
         );
