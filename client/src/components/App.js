985eb2de2a3165acdd426dbecfbaf27dc286d378
Luiz Claudio <luizcns18@gmail.com>
2017-12-12 11:25:04 -0300

    test: Update app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Home from './Home';
+import Category from './Category';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Categories</h1>
-        <h2>React</h2>
-        <h2>Redux</h2>
-        <h2>Udacity</h2>
+        <Home />
+        <Category />
       </div>
     );
   }
--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -4,7 +4,9 @@ class Category extends Component {
 
     render(){
         return (
-            <div></div>
+            <div>
+                <h1>Category</h1>
+            </div>
         );
     }
 
