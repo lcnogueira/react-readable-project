1bba659906358a9439a8ce4192923a38d2695ce9
Luiz Claudio <luizcns18@gmail.com>
2017-12-16 22:06:53 -0300

    refactor: Format date

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { List, Container } from 'semantic-ui-react';
+import { formatDate } from '../utils/helper';
 
 const PostList = (props) => {
 
@@ -11,7 +12,7 @@ const PostList = (props) => {
                     <List.Item key={post.id} >
                         <List>
                             <List.Item icon='talk' content={post.title} />
-                            <List.Item icon='checked calendar' content={post.timestamp} />
+                            <List.Item icon='checked calendar' content={formatDate(post.timestamp)} />
                             <List.Item icon='user outline' content={post.author} />
                             <List.Item icon= {post.voteScore>=0?'thumbs up':'thumbs down'} content={post.voteScore} />
                             <List.Item icon='commenting outline' content={post.commentCount>0 ? post.commentCount : '0'} />
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,5 +1,11 @@
+import * as moment from "moment";
+
 export function capitalize (str = '') {
     return typeof str !== 'string'
       ? ''
       : str[0].toUpperCase() + str.slice(1)
   }
+
+export function formatDate(timestamp) {
+    return moment(timestamp).format('Do MMM YYYY');
+}
