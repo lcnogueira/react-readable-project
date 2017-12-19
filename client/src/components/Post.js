d1db180d582e53dbfbc65966163ff52ce1d2f933
Luiz Claudio <luizcns18@gmail.com>
2017-12-18 21:30:59 -0300

    refactor: Update post component

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { formatDate } from '../utils/helper';
+import { List, Icon } from 'semantic-ui-react';
 
 class Post extends Component {
 
     render(){
+        const { post } = this.props;
+
         return (
-            <div></div>
+            <List size='large'>
+                <List.Item icon='talk' content={post.title}/>
+                <List.Item 
+                    icon='info circle' 
+                    content={formatDate(post.timestamp) + ' - Author: ' + post.author + ' - Comments: ' + post.commentCount}
+                />
+                <List.Item>
+                    <Icon link name='thumbs up'/>
+                    {post.voteScore}
+                    <Icon link name='thumbs down'/>
+                </List.Item>
+                <List.Item >
+                    <Icon link name='edit' />
+                    <Icon link name='trash'/>
+                </List.Item>
+            </List>
         );
     }
 
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { List, Container } from 'semantic-ui-react';
-import { formatDate } from '../utils/helper';
+import Post from './Post';
 
 const PostList = (props) => {
 
     const { posts } = props;
     return (
         <Container>
-            <List size="huge" verticalAlign="middle" inverted={true}>
+            <List size="huge" verticalAlign="middle">
                 {posts && posts.map((post) => (
                     <List.Item key={post.id} >
-                        <List>
-                            <List.Item icon='talk' content={post.title} />
-                            <List.Item icon='checked calendar' content={formatDate(post.timestamp)} />
-                            <List.Item icon='user outline' content={post.author} />
-                            <List.Item icon= {post.voteScore>=0?'thumbs up':'thumbs down'} content={post.voteScore} />
-                            <List.Item icon='commenting outline' content={post.commentCount>0 ? post.commentCount : '0'} />
-                        </List>
+                        <Post post={post} key={post.id}/>
                     </List.Item>
                 ))}
             </List>
